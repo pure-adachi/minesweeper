11bb6e418041d377c3e835ff09e026e389caf0a8
pure-adachi <takuya_adachi@pure-system.com>
2020-01-03 21:59:16 +0900

    Updates

---
+++ precache-manifest.1a7685299775fa464dfd717d29804391.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f743b02723c50a7e5574f531b32500",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1927a82701a19e58f580",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bd7483212da1563739c2",
+    "url": "/static/js/2.23d34c09.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.23d34c09.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1927a82701a19e58f580",
+    "url": "/static/js/main.e112a24d.chunk.js"
+  },
+  {
+    "revision": "72cfcfed00a552795b92",
+    "url": "/static/js/runtime-main.65dc0493.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
