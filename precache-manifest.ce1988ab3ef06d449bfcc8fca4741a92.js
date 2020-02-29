ba1e6ec9ed5e55b1c5983fb62ec4e1d3d50d02cf
pure-adachi <takuya_adachi@pure-system.com>
2020-02-29 21:54:21 +0900

    Updates

--- precache-manifest.ce1988ab3ef06d449bfcc8fca4741a92.js
+++ precache-manifest.ce1988ab3ef06d449bfcc8fca4741a92.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "113050b1200dc236f268b1b5ae6db66c",
+    "revision": "981b31f4e6512426dcca01aa7c9e9ece",
     "url": "/minesweeper/index.html"
   },
   {
-    "revision": "8bdb9212326ae491494f",
+    "revision": "0462451a10a9dfb8c622",
     "url": "/minesweeper/static/css/main.36b0a12f.chunk.css"
   },
   {
-    "revision": "631f469357ebcddb691d",
-    "url": "/minesweeper/static/js/2.5ec89ecd.chunk.js"
+    "revision": "e54d4f1758abbc7a45eb",
+    "url": "/minesweeper/static/js/2.6b554691.chunk.js"
   },
   {
     "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
-    "url": "/minesweeper/static/js/2.5ec89ecd.chunk.js.LICENSE"
+    "url": "/minesweeper/static/js/2.6b554691.chunk.js.LICENSE"
   },
   {
-    "revision": "8bdb9212326ae491494f",
-    "url": "/minesweeper/static/js/main.96620dec.chunk.js"
+    "revision": "0462451a10a9dfb8c622",
+    "url": "/minesweeper/static/js/main.03311663.chunk.js"
   },
   {
     "revision": "81fac3eb777183ec555d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/minesweeper/precache-manifest.40bed3722d82d987e839bc4860389995.js"
+  "/minesweeper/precache-manifest.ce1988ab3ef06d449bfcc8fca4741a92.js"
 );
 
 self.addEventListener('message', (event) => {
